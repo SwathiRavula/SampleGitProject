6b9b388494115f916a7763a5274c9b6f56c86c5e
swathi <swathikalavakunta@gmail.com>
2021-10-07 18:07:53 +0530

    Comminting sample .java file

---
+++ Person.java
@@ -0,0 +1,36 @@
+package simple;
+
+import java.util.Objects;
+
+public class Person {
+	String name;
+	int id;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		return id == other.id && Objects.equals(name, other.name);
+	}
+	
+}
